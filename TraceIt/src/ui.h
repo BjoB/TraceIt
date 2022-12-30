a928519e2f45d513d0f56530fc47d1908f8fb115
Bat <bjoern.barschtipan@gmail.com>
2022-12-30 15:27:16 +0100

    Extract ui code to separate class

---
+++ TraceIt/src/ui.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+namespace Ui {
+
+struct Config {
+    const char* name;
+    int window_width;
+    int window_height;
+};
+
+class App {
+   public:
+    App(const Config& config);
+    App(const App&) = delete;
+    App& operator=(const App) = delete;
+    virtual ~App();
+
+    void run();
+    GLFWwindow* getWnd() { return m_window; }
+
+   private:
+    void init(const char* name, int window_width, int window_height);
+    void cleanup();
+
+    GLFWwindow* m_window = nullptr;
+    bool m_running = false;
+};
+
+}  // namespace Ui
