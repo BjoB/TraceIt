bfa5840444301f5b9461871256e6503ba0b18c9f
Bat <bjoern.barschtipan@gmail.com>
2023-01-21 18:44:41 +0100

    Add hit method for sphere and complete scene

--- TraceIt/src/scene.h
+++ TraceIt/src/scene.h
@@ -1,16 +1,38 @@
 #pragma once
 
-#include "log.h"
-
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
+#include <glm/glm.hpp>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <type_traits>
 #include <unordered_map>
-#include <vector>
 #include <variant>
+#include <vector>
+
+#include "log.h"
+
+using namespace glm;
+using color = vec4;
+
+class Ray {
+   public:
+    using Scalar = vec3::value_type;
+
+    Ray(const vec3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+    vec3 at(Scalar t) const { return orig + t * dir; }
+
+   public:
+    vec3 orig;
+    vec3 dir;
+};
+
+struct RayHitRecord {
+    vec3 position;
+    vec3 normal;
+    color color;
+    float t;
+};
 
 enum ObjectType {
     kPlane,
@@ -24,23 +46,55 @@ enum MaterialType { Lambertian, Metal };
 struct Object {
     virtual ~Object() = default;
 
-    // virtual const char* objTypeName() const = 0;
-    // virtual void render() = 0;
+    virtual bool hit(const Ray& ray, float t_min, float t_max, RayHitRecord& rec) const = 0;
 
     std::string name;
-    glm::vec3 position;
-    glm::vec4 color;
+    vec3 position;
+    color color;
     MaterialType material;
 };
 
-struct Plane : Object {};
+struct Plane : Object {
+    bool hit(const Ray& ray, float t_min, float t_max, RayHitRecord& rec) const override { return false; }
+};
 
 struct Cube : Object {
+    bool hit(const Ray& ray, float t_min, float t_max, RayHitRecord& rec) const override { return false; }
+
     float width;
     float height;
 };
 
 struct Sphere : Object {
+    bool hit(const Ray& ray, float t_min, float t_max, RayHitRecord& rec) const override {
+        const vec3 sphere_center_to_ray_orig = ray.orig - position;
+        const auto a = dot(ray.dir, ray.dir);
+        const auto half_b = dot(sphere_center_to_ray_orig, ray.dir);
+        const auto c = dot(sphere_center_to_ray_orig, sphere_center_to_ray_orig) - radius * radius;
+        const auto discriminant = half_b * half_b - a * c;
+        if (discriminant < 0) {
+            rec.t = -1.f;
+            return false;
+        }
+        const auto sqrt_discr = sqrt(discriminant);
+
+        auto inAllowedRange = [&](float t) { return (t < t_min || t > t_max) ? false : true; };
+
+        auto t = (-half_b - sqrt_discr) / a;
+        if (!inAllowedRange(t)) {
+            t = (-half_b + sqrt_discr) / a;
+            if (!inAllowedRange(t)) {
+                return false;
+            }
+        }
+
+        rec.t = t;
+        rec.position = ray.at(t);
+        rec.normal = (rec.position - position) / radius;  // as unit vector
+        rec.color = color;
+        return true;
+    }
+
     float radius;
 };
 
@@ -76,6 +130,21 @@ class Scene {
     }
 
     auto& objects() const { return m_objects; }
+
+    bool hit(const Ray& ray, double t_min, double t_max, RayHitRecord& rec) const {
+        RayHitRecord temp_rec;
+        bool hit_any_object = false;
+        auto closest_t = t_max;
+
+        for (auto object_variant : m_objects) {
+            if (std::visit([&](auto&& obj) { return obj.hit(ray, t_min, closest_t, temp_rec); }, *object_variant)) {
+                hit_any_object = true;
+                closest_t = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+        return hit_any_object;
+    }
 
     auto availableObjectTypes() const {
         std::vector<std::string> obj_names;
